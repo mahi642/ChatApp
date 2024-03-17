774e80d79072babcf16f461118d0b9ec34a4ead3
maheshbalabadra <maheshbalabadra186@gmail.com>
2024-03-18 00:56:20 +0530

    Login,signup

---
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Conversation = () => {
+  return (
+    <>
+    <div className = "flex gap-2 itmes-center hover:bg-sky-500 rounded  p-2 py-1 cursor-pointer">
+      <div className = 'avatar online'>
+        <div className='w-12 rounded-full '>
+            <img src = "" alt ="user image"/>
+        </div>
+      </div>
+    </div>
+    <div></div>
+    </>
+  )
+}
+
+export default Conversation
---
+++ frontend/src/pages/signup/SignUp.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import GenderCheckbox from "./GenderCheckbox";
+
+const SignUp = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-grey-300 ">
+          SignUp
+          <span className=" text-blue-500"> ChatApp</span>
+        </h1>
+        <form>
+          <label className="label p-2">
+            <span className="text-base label-text ">Full Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Joh Doe"
+            className="w-full input input-rounded h-10"
+          />
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text ">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="John Doe"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text ">Password </span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter the password "
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text ">Confirm Password </span>
+            </label>
+            <input
+              type="password"
+              placeholder="Confirm password  "
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <GenderCheckbox/>
+
+          <a
+            href="#"
+            className="text-sm hover:underline hover:text-blue-600 mt-4 inline-block "
+          >
+            Already have an account?
+          </a>
+
+          <div>
+            <button className="btn btn-block btn-sm mt-2 ">Signup</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
