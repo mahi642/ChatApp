81b270b910e0cfa6da775e64849b4b8df26cd845
maheshbalabadra <maheshbalabadra186@gmail.com>
2024-03-18 23:15:09 +0530

    added routes and context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,28 @@ import {Toaster} from "react-hot-toast"
 
 
 
-import { Routes,Route } from "react-router-dom";
+import { Routes,Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
-  return <div className="p-4 h-screen flex items-center justify-center">
-    <Routes>
-      <Route path = "/" element = {<Home/>}/>
-      <Route path = "/login" element = {<Login/>}/>
-      <Route path = "/signup" element = {<SignUp/>}/>
-    </Routes>
-    <Toaster/>
-  </div>
+
+  const {authUser} = useAuthContext();
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route  path = "/" element = {authUser ?<Home/> :<Navigate to = {"/login"} />}/>
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
+      </Routes>
+      <Toaster />
+    </div>
+  );
 }
 
 
---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,40 @@
+import toast from "react-hot-toast";
+import { useAuthContext} from "../context/AuthContext";
+import { useState } from "react";
+const useLogout = ()=>{
+
+    const [loading,setLoading] = useState(false);
+    const {setAuthUser} = useAuthContext()
+    const logout = async ()=>{
+        setLoading(true);
+        try {
+            const res = await fetch("/api/auth/logout",{
+                method:"POST",
+                headers:{"Content-TYpe" :"application/json"}
+
+            })
+
+            const data = res.json();
+            if(data.error){
+                throw new Error(data.error)
+            }
+            localStorage.removeItem("chat-user")
+            setAuthUser(null)
+
+
+
+            
+        } catch (error) {
+            toast.error(error.message)
+            
+        }
+        finally{
+            setLoading(false);
+        }
+    }
+
+    return {loading,logout}
+
+}
+
+export default useLogout ;
